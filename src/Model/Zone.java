d101a0aa4321ed5e75765c737158a891584be4b1
yougzhao <yougzhao@gmail.com>
2017-02-19 22:56:06 +0800

    add

--- src/Model/Zone.java
+++ src/Model/Zone.java
@@ -15,16 +15,26 @@ public class Zone {
 	public double minLon;
 	public double maxLat;
 	public double minLat;
-	public int n;
+	public int num;
 	public List<Double> lons;
 	public List<Double> lats;
 	public Zone(){
 		lons = new ArrayList<Double>();
 		lats = new ArrayList<Double>();
+		maxLon = Double.MAX_VALUE;
+		minLon = Double.MIN_VALUE;
+		maxLat = Double.MAX_VALUE;
+		minLat = Double.MIN_VALUE;
+		num=0;
 	}
 	public Zone(int id){
 		this.id = id;
 		lons = new ArrayList<Double>();
 		lats = new ArrayList<Double>();
+		maxLon = Double.MAX_VALUE;
+		minLon = Double.MIN_VALUE;
+		maxLat = Double.MAX_VALUE;
+		minLat = Double.MIN_VALUE;
+		num=0;
 	}
 }
--- src/Model/ZoneMap.java
+++ src/Model/ZoneMap.java
@@ -10,22 +10,56 @@ import java.util.List;
  */
 public class ZoneMap {
 	public List<Zone> zoneList;
+	public int num;
 	public double maxLon;
 	public double minLon;
 	public double maxLat;
 	public double minLat;
 	public ZoneMap(){
 		zoneList = new ArrayList<Zone>();
+		maxLon = Double.MAX_VALUE;
+		minLon = Double.MIN_VALUE;
+		maxLat = Double.MAX_VALUE;
+		minLat = Double.MIN_VALUE;
+		num=0;
 	}
 	//初始化，载入交通小区数据
 	public void init()throws Exception{
 		BufferedReader br = new BufferedReader(new FileReader(".\\input\\Zone\\ZoneMap.txt"));
+		System.out.println("Start init...");
 		String af;
 		String[] afs;
 		while((af=br.readLine())!=null){
-			
+			afs = af.split(",");
+			Zone newZone = new Zone(Integer.parseInt(afs[0]));
+			newZone.num = Integer.parseInt(afs[4]);
+			for(int i=0;i<newZone.num;i+=2){
+				double newLon = Double.parseDouble(afs[5+i]);
+				double newLat = Double.parseDouble(afs[6+i]);
+				newZone.lons.add(newLon);
+				newZone.lats.add(newLat);
+				if(newLon>newZone.maxLon)
+					newZone.maxLon = newLon;
+				if(newLon<newZone.minLon)
+					newZone.minLon = newLon;
+				if(newLat>newZone.maxLat)
+					newZone.maxLat = newLat;
+				if(newLat<newZone.minLat)
+					newZone.minLat = newLat;
+				if(newLon>this.maxLon)
+					this.maxLon = newLon;
+				if(newLon<this.minLon)
+					this.minLon = newLon;
+				if(newLat>this.maxLat)
+					this.maxLat = newLat;
+				if(newLat<this.minLat)
+					this.minLat = newLat;
+				this.zoneList.add(newZone);
+				num+=1;
+			}
 		}
 		br.close();
+		System.out.println("Finish init...");
 	}
 	public List<Zone> getZoneList(){
 		return zoneList;
